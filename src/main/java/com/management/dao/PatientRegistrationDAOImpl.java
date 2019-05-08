2768cb2d30c68432f4ca2ce7e06b0cac4d0446bd
Pallavi PR <palluramachandran@gmail.com>
2019-05-09 01:27:21 +0530

    Dao Layer Changes and JSP Changes

--- src/main/java/com/management/dao/PatientRegistrationDAOImpl.java
+++ src/main/java/com/management/dao/PatientRegistrationDAOImpl.java
@@ -1,11 +1,14 @@
 package com.management.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.management.model.Patient;
@@ -21,40 +24,62 @@ public class PatientRegistrationDAOImpl implements PatientRegistrationDAO {
 	
 	@Override
 	public List<Patient> getAllPatients() {
-		Patient p1=new Patient(1001,"pallavi",23,"9867543423","bangalore");
-		Patient p2=new Patient(1002,"sharath",27,"9456888887","kanhagad");
-		List<Patient> patientList=new ArrayList<>();
-		patientList.add(p1);
-		patientList.add(p2);
+		String sql="select * from HOSPITAL.PATIENT";		
+		List<Patient>patientList=jdbcTemplate.query(sql, new RowMapper<Patient>() {
+			@Override
+			public Patient mapRow(ResultSet rs,int rowNum) throws SQLException {
+				Patient patient = new Patient();
+				patient.setId(rs.getInt("ID"));
+				patient.setName(rs.getString("NAME"));
+				patient.setAge(rs.getInt("AGE"));
+				patient.setContactNo(rs.getString("CONTACTNO"));
+				patient.setAddress(rs.getString("ADDRESS"));
+				return patient;
+			}
+		});
 		
 		return patientList;
 	}
 
 	@Override
 	public Patient viewPatient(int id) {
-		Patient p1=new Patient(1001,"pallavi",23,"9867543423","bangalore");
-		return p1;
+		String sql="select * from HOSPITAL.PATIENT WHERE ID=?";	
+		Patient patient=jdbcTemplate.queryForObject(sql, new RowMapper<Patient>() {
+			@Override
+			public Patient mapRow(ResultSet rs,int rowNum) throws SQLException {
+				Patient patient = new Patient();
+				patient.setId(rs.getInt("ID"));
+				patient.setName(rs.getString("NAME"));
+				patient.setAge(rs.getInt("AGE"));
+				patient.setContactNo(rs.getString("CONTACTNO"));
+				patient.setAddress(rs.getString("ADDRESS"));
+				return patient;
+			}
+		},id);
+		
+		return patient;
 		
 	}
 
 	@Override
 	public void addPatient(Patient patient) {
-		String sql="insert into HOSPITAL.PATIENT(id,name,age,contactno,address) values(?,?,?,?,?)";
-		jdbcTemplate.update(sql,patient.getId(),patient.getName(),patient.getAge(),patient.getContactNo(),patient.getAddress());
-		System.out.println(patient.getId()+""+patient.getName()+""+patient.getAge()+""+patient.getContactNo()+""+patient.getAddress());
+		String insertSql="insert into HOSPITAL.PATIENT(id,name,age,contactno,address) values(?,?,?,?,?)";
+		jdbcTemplate.update(insertSql,patient.getId(),patient.getName(),patient.getAge(),patient.getContactNo(),patient.getAddress());
 		
 	}
 
 	@Override
 	public void updatePatient(Patient patient) {
-		System.out.println(patient.getId()+""+patient.getName()+""+patient.getAge()+""+patient.getContactNo()+""+patient.getAddress());
+		String updateSql="update HOSPITAL.PATIENT SET ID=?,NAME=?,AGE=?,CONTACTNO=?,ADDRESS=? WHERE ID=?";
+		jdbcTemplate.update(updateSql,patient.getId(),patient.getName(),patient.getAge(),patient.getContactNo(),patient.getAddress(),patient.getId());
 		
 		
 	}
 
 	@Override
 	public void deletePatient(int id) {
-		System.out.println("patient deleted"+id);
+		String deleteSql="delete from HOSPITAL.PATIENT WHERE ID=?";
+		jdbcTemplate.update(deleteSql,id);
 	}
 
 
